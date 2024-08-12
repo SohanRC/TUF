ab39bc5275c68a96a5f97846c7f1995caccd15c1
SOHAN <sohanroychowdhury7@gmail.com>
2024-08-12 10:24:22 +0530

    Change 3 MYSQL2

--- Backend/config/dbConnect.js
+++ Backend/config/dbConnect.js
@@ -1,4 +1,4 @@
-import mysql from "mysql"
+import mysql from "mysql2"
 import { config } from "dotenv";
 config();
 //ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'your_current_password';
@@ -8,6 +8,15 @@ const db = mysql.createConnection({
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB,
     port : process.env.MYSQL_PORT
+})
+db.connect((err) => {
+    if (err) {
+        console.log(err)
+        console.log("Cannot Connect !");
+        db.end();
+        return;
+    }
+    console.log("Connection Success !");
 })
 
 export default db;
